d318bece3d00504c3211cc7d296af089e7d742fd
ramseya2k <ramseya2k@gmail.com>
2024-02-24 14:50:10 -0800

    hw7

--- hw7/hw7.c
+++ hw7/hw7.c
@@ -6,7 +6,8 @@
 #define ROWS	240
 #define COLUMNS	240
 #define PI 3.14159265358979323846
-#define sqr(x) ((x)*(x))
+#define sqr(x)  ((x)*(x))
+
 void clear( unsigned char image[][COLUMNS] );
 void header( int row, int col, unsigned char head[32] );
 
@@ -16,136 +17,131 @@ int main( int argc, char** argv )
 	FILE*			fp;
 	unsigned char	image[ROWS][COLUMNS], head[32];
 	char			filename[50], ch;
-	float           S[3][3] = { { 0, 0, 1 }, { 1/sqrt(3), 1/sqrt(3), 1/sqrt(3) }, { 1, 0, 0 } }, V[] = { 0, 0, 1 }, H[3], N[3], E[ROWS][COLUMNS];
-	float			R[3] = { 50, 10, 100 }, M[3] = { 1, 0.1, 10000 }, A[3] = { 0.5, 0.1, 1 };
-	float			r, m, a, s[3], alpha, x, y, costheta, len, z, p, q, lambert, irradiance;
+	float           V[] = { 0, 0, 1 }, H[3], N[3], E[ROWS][COLUMNS];
+	float			r, m, a, s[3], alpha, x, y, costheta, len, z, p, q;
 
 	header ( ROWS, COLUMNS, head );
 	
 	for ( k = 0; k < 9; k ++ )
 	{
 		clear(image);
-        switch(k)
-        {
-            case 0:
-                strcpy(filename, "sphere-1.ras");
-                s[0] = 0;
-                s[1] = 0;
-                s[2] = 1;
-                r = 50;
-                a = 0.5;
-                m = 1;
-                break;
-            case 1:
-                strcpy(filename, "sphere-2.ras");
-                s[0] = 1/sqrt(3);
-                s[1] = 1/sqrt(3);
-                s[2] = 1/sqrt(3);
-                r = 50;
-                a = 0.5;
-                m = 1;
-                break;
-            case 2:
-                strcpy(filename, "sphere-3.ras");
-                s[0] = 1;
-                s[1] = 0;
-                s[2] = 0;
-                r = 50;
-                a = 0.5;
-                m = 1;
-                break;
-            case 3:
-                strcpy(filename, "sphere-4.ras");
-                s[0] = 0;
-                s[1] = 0;
-                s[2] = 1;
-                r = 10;
-                a = 0.5;
-                m = 1;
-                break;
-            case 4:
-                strcpy(filename, "sphere-5.ras");
-                s[0] = 0;
-                s[1] = 0;
-                s[2] = 1;
-                r = 100;
-                a = 0.5;
-                m = 1;
-                break;
-            case 5:
-                strcpy(filename, "sphere-6.ras");
-                s[0] = 0;
-                s[1] = 0;
-                s[2] = 1;
-                r = 50;
-                a = .1;
-                m = 1;
-                break;
-            case 6:
-                s[0] = 0;
-                s[1] = 0;
-                s[2] = 1;
-                r = 50;
-                a = 1;
-                m = 1;
-                strcpy(filename, "sphere-7.ras");
-                break;
-            case 7:
-                s[0] = 0;
-                s[1] = 0;
-                s[2] = 1;
-                r = 50;
-                a = 0.5;
-                m = 0.1;
-                strcpy(filename, "sphere-8.ras");
-                break;
-            case 8:
-                s[0] = 0;
-                s[1] = 0;
-                s[2] = 1;
-                r = 50;
-                a = 0.5;
-                m = 10000;
-                strcpy(filename, "sphere-9.ras");
-                break;
-            default:
-                printf("Nothing to do here\n");
-                break;
-        }
-        
-        x = COLUMNS / 2;
-        y = ROWS / 2;
-        float vm = 0;
-        for(i=0; i < ROWS; i++)
-        {
-            for(j=0; j < COLUMNS; j++)
-            {
-                if((sqr(r) - sqr(j-x) + sqr(i-y)) > 0)
-                {
-                    z = sqrt((sqr(r) - sqr(j-x) + sqr(i-y)));
-                    p = -(j-x) / z;
-                    q = (i-y) / z;
-
-
-                    N[0] = -p / sqrt(sqr(p) + q * q+1);
-                    N[1] = -q / sqrt(sqr(p) + q * q+1);
-                    N[2] = 1 / sqrt(sqr(p) + q * q+1);
-
-                    lambert = (N[0] * s[0]) + (N[1] * s[1]) + (N[2] * s[2]);
-                    if(lambert >= 0)
-                    {
-                        vm = sqrt((sqr(V[0] + s[0])) + (sqr(V[1] + s[1])) + (sqr(V[2] + s[2])));
-                        H[0] = (V[0] + s[0]) / vm;
-                        H[1] = (V[1] + s[1]) / vm;
-                        H[2] = (V[2] + s[2]) / vm;
-                        alpha = acos((N[0] * H[0]) + (N[1] * H[1]) + (N[2] * H[2]));
-                        irradiance = a * lambert + (1-a) * (exp(-(sqr(alpha/m))));
-                        image[i][j] = irradiance * 255;
-                    }
-
-                }
-            }
-        }
+		switch(k)
+		{
+			case 0:
+				strcpy(filename, "sphere-1.ras");
+				s[0] = 0;
+				s[1] = 0;
+				s[2] = 1;
+				r = 50;
+				a = 0.5;
+				m = 1;
+				break;
+			case 1:
+				strcpy(filename, "sphere-2.ras");
+				s[0] = 1 / sqrt(3);
+				s[1] = 1 / sqrt(3);
+				s[2] = 1 / sqrt(3);
+				r = 50;
+				a = 0.5;
+				m = 1;
+				break;
+			case 2:
+				strcpy(filename, "sphere-3.ras");
+				s[0] = 1;
+				s[1] = 0;
+				s[2] = 0;
+				r = 50;
+				a = 0.5;
+				m = 1;
+				break;
+			case 3:
+				strcpy(filename, "sphere-4.ras");
+				s[0] = 0;
+				s[1] = 0;
+				s[2] = 1;
+				r = 10;
+				a = 0.5;
+				m = 1;
+				break;
+			case 4:
+				strcpy(filename, "sphere-5.ras");
+				s[0] = 0;
+				s[1] = 0;
+				s[2] = 1;
+				r = 100;
+				a = 0.5;
+				m = 1;
+				break;
+			case 5:
+				strcpy(filename, "sphere-6.ras");
+				s[0] = 0;
+				s[1] = 0;
+				s[2] = 1;
+				r = 50;
+				a = 0.1;
+				m = 1;
+				break;
+			case 6:
+				strcpy(filename, "sphere-7.ras");
+				s[0] = 0;
+				s[1] = 0;
+				s[2] = 1;
+				r = 50;
+				a = 1;
+				m = 1;
+				break;
+			case 7:
+				strcpy(filename, "sphere-8.ras");
+				s[0] = 0;
+				s[1] = 0;
+				s[2] = 1;
+				r = 50;
+				a = 0.5;
+				m = 0.1;
+				break;
+			case 8:
+				strcpy(filename, "sphere-9.ras");
+				s[0] = 0;
+				s[1] = 0;
+				s[2] = 1;
+				r = 50;
+				a = 0.5;
+				m = 10000;
+				break;
+			default:
+				break;
+		}
+		
+		// circle drawing
+		x = COLUMNS / 2;
+		y = ROWS /2;
+		float vmag = 0;
+		for(i=0; i<ROWS;i++)
+		{
+			for(j=0; j<COLUMNS; j++)
+			{
+				if((sqr(r) - (sqr(j-x) + sqr(i-y)) > 0))
+				{
+					z = sqrt(sqr(r) - (sqr(j-x) + sqr(i-y)));
+					p = -(j-x) / z;
+					q = (i-y) / z;
+					N[0] = -p / sqrt(sqr(p) + sqr(q) + 1);
+					N[1] = -q / sqrt(sqr(p) + sqr(q) + 1);
+					N[2] = 1 / sqrt(sqr(p) + sqr(q) + 1);
+					float lambert = (N[0] * s[0]) + (N[1] * s[1]) + (N[2] * s[2]);
+					if(lambert >= 0)
+					{
+						vmag = sqrt(sqr(V[0] + s[0]) + sqr(V[1] + s[1]) + sqr(V[2] + s[2]));
+						H[0] = (V[0] + s[0]) / vmag;
+						H[1] = (V[1] + s[1]) / vmag;
+						H[2] = (V[2] + s[2]) / vmag;
+						alpha = acos((N[0] * H[0]) + (N[1] * H[1]) + (N[2] * H[2]));
+						float irradiance = a * lambert + (1-a) * (exp(-(alpha/m) * (alpha/m)));
+						image[i][j] = irradiance * 255;
+					}
+				}
+			}
+		}
 		
 		// IMPORTANT: to let codes compile on Gradescope Autograder, please don't change code below.(line60 - line 137) 
 		// FYI, line 60 - line 73 are inside for loop "for (k = 0; k < 9; k ++)"
@@ -160,8 +156,8 @@ int main( int argc, char** argv )
 		  fwrite( image[i], sizeof(char), COLUMNS, fp );
 		fclose( fp );
 	}
-    printf("Done!\n");
-	//printf("Press any key to exit: ");
+
+	printf("Finished!\n");
 	//gets ( &ch );
 	return 0;
 }
